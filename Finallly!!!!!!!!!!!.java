4592301096dac662abff4f4bed61a996d1f60b1f
okboka <43502494+okboka@users.noreply.github.com>
2018-10-20 11:24:46 -0700

    Update Finallly!!!!!!!!!!!.java

--- Finallly!!!!!!!!!!!.java
+++ Finallly!!!!!!!!!!!.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculatorActual{
+public class not{
   public static void main (String [] args) {
 
       System.out.println("Enter number: ");
@@ -9,12 +9,12 @@ public class CalculatorActual{
       System.out.println("this is your number: " + x);
       System.out.println("What do you want to do with it?");
       String letters = scanner.nextLine();
-      System.out.println("Do you want to " + letters + " it? Ok then. Put in your number you want to" + letters + "by.");
+      System.out.println("Do you want to " + letters + " it? Ok then. Put in your number you want to " + letters + " by.");
       String y = scanner.nextLine();
       System.out.println("Your awnser is one of these");
       System.out.println();
-      System.out.println(x(y));
+      System.out.println(x + "/" + y);
       System.out.println(x + y);
-      System.out.println(x - y);
+      System.out.println(x + "-" + y);
   }
 }
